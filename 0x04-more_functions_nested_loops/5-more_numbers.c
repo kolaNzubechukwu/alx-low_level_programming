36dc9506e6df4195205434c2dc1352677eb4b652
kolaNzubechukwu <fransiscomoma@gmail.com.com>
2023-07-20 15:18:02 -0700

    corrected something i task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,11 +14,10 @@ void more_numbers(void)
 			if (j <= 10)
 			{
 				_putchar((j / 10) + '0');
-				_putchar((j % 10) + '0');
 			}
 			else
 			{
-				_putchar(j + 'o');
+				_putchar((j % 10) + '0');
 			}
 
 		}
