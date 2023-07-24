8ecc6917ed42f731415fac03f793296b4b29a142
kolaNzubechukwu <fransiscomoma@gmail.com.com>
2023-07-24 08:26:54 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * swap_int - swaps a and b
+ * @a: val to swap
+ * @b: val to swap
+ */
+void swap_int(int *a, int *b)
+{
+	int tmp;
+	*a = tmp;
+	*b = *a;
+	*b = tmp;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,6 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _putchar(char c);
 #endif
