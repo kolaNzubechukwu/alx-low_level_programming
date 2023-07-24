3a87bdd630b206dd64da373a5474818412a5013c
kolaNzubechukwu <fransiscomoma@gmail.com.com>
2023-07-24 11:10:44 -0700

    added the rest

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,17 +1,26 @@
-#include <stdio.h>
-/**
- * _puts - print a string inputed to stdo
- * standard out put
- * @str: the string to output
- */
-void _puts(char *str)
-{
-	int i = 0;
+#include "main.h"
 
-	while (str[i] != '\0')
+/**
+ * puts2 - prints one char out of 2 of a string
+ * followed by a new line
+ * @str: string to print the chars from
+ */
+void puts2(char *str)
+{
+	int len, i;
+
+	len = 0;
+
+	while (str[len] != '\0')
 	{
-		printf("%c", str[i]);
-		i++;
+		len++;
 	}
-	putchar('\n');
+
+	for (i = 0; i < len; i += 2)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
 }
+
