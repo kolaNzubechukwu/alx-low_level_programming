11690d9adf10308d5fb0f7fae3b622bf06ca9040
kolaNzubechukwu <fransiscomoma@gmail.com.com>
2023-08-10 18:08:00 -0700

    bash

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+void *malloc_checked(unsigned int b);
+int _putchar(char c);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+
+#endif
